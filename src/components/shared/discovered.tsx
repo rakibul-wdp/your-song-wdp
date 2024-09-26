73b0872686d8cc285d49abafa9484b9f946bfb97
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-27 04:05:00 +0600

    home page discovered section done

--- src/components/shared/discovered.tsx
+++ src/components/shared/discovered.tsx
@@ -9,24 +9,30 @@ export const Discovered: FC = () => {
     <Container
       className={cn(
         "py-16 md:py-[51.47px]",
-        "flex items-center justify-center"
+        "flex flex-col md:flex-row items-center justify-center gap-16"
       )}
     >
-      <div>
-        <Title variant="H4">Are You an Artist? Get Discovered.</Title>
-        <P variant="P2" className="leading-8 font-light text-[#A1A1A2]">
+      <div className="flex-1">
+        <Title variant="H4" className="text-center md:text-start">
+          Are You an Artist? Get Discovered.
+        </Title>
+        <P
+          variant="P2"
+          className="leading-8 font-light text-[#A1A1A2] mt-6 mb-10"
+        >
           Join our platform and earn money doing what you love. Our supporters
           care about producing quality songs with broad commercial appeal that
-          you’ll be proud to claim. <br /> With decades of industry experience
-          and a user-friendly platform, we make it easy for you to get noticed,
-          promote your music, track your royalties, and make life-long fans.
+          you’ll be proud to claim. <br /> <br /> With decades of industry
+          experience and a user-friendly platform, we make it easy for you to
+          get noticed, promote your music, track your royalties, and make
+          life-long fans.
         </P>
-        <Button variant="default" size="default">
+        <Button variant="default" size="default" className="w-full sm:w-fit">
           Apply now
         </Button>
       </div>
 
-      <div>
+      <div className="flex-1">
         <Image
           src="/images/discovered.svg"
           width={693}

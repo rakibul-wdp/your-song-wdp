189c7bd30bea1f23df0494f2a95b379dac16e730
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-27 04:35:00 +0600

    shared testimonials section done

--- src/components/shared/testimonials.tsx
+++ src/components/shared/testimonials.tsx
@@ -45,10 +45,12 @@ export const testimonials: ITestimonials[] = [
 
 export const Testimonials: FC = () => {
   return (
-    <Container className="py-16 md:py-[120px]">
-      <SubTitle variant="H5">See What Supporters Are Saying</SubTitle>
+    <Container className="py-16 md:py-[120px] flex flex-col items-center justify-center gap-10 md:gap-20">
+      <SubTitle variant="H5" className="text-center">
+        See What Supporters Are Saying
+      </SubTitle>
 
-      <div className="pt-10 pb-[84px] md:py-20">
+      <div>
         <Carousel
           opts={{
             align: "start",
@@ -57,8 +59,11 @@ export const Testimonials: FC = () => {
         >
           <CarouselContent>
             {testimonials.map(({ id, description }) => (
-              <CarouselItem key={id} className="basis-1/1">
-                <div>
+              <CarouselItem
+                key={id}
+                className="basis-[45%] lg:basis-[33%] w-full mx-3 flex flex-col items-start justify-start"
+              >
+                <div className="w-full flex items-center justify-between mb-6">
                   <Image
                     src="/icons/quote.svg"
                     width={34}
@@ -77,20 +82,22 @@ export const Testimonials: FC = () => {
                     ))}
                   </div>
                 </div>
-                <P variant="P3" className="font-light w-[25%]">
-                  {description}
-                </P>
-                <P variant="P2" className="leading-7 text-[#6E6E72]">
-                  @MimiG321
-                </P>
                 <div>
+                  <P variant="P3" className="font-light">
+                    {description}
+                  </P>
+                  <P variant="P2" className="leading-7 text-[#6E6E72] my-6">
+                    @MimiG321
+                  </P>
+                </div>
+                <div className="flex items-center justify-center gap-4">
                   <Image
                     src="/icons/play-avatar.svg"
                     width={40}
                     height={40}
                     alt="play avatar"
                   />
-                  <div className="flex items-center justify-between">
+                  <div className="flex flex-col items-start justify-between gap-1">
                     <P variant="P3" className="leading-5">
                       Baby, i’m gonna leave you
                     </P>
@@ -102,12 +109,12 @@ export const Testimonials: FC = () => {
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
+          <CarouselPrevious className="border-none" />
+          <CarouselNext className="border-none" />
         </Carousel>
       </div>
 
-      <Button variant="default" size="default">
+      <Button variant="default" size="default" className="w-full sm:w-fit">
         Create Your song
       </Button>
     </Container>

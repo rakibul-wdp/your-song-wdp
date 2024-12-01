90ab26426645b8d8b1f84c4b24bbbbfc60eb789f
rakibul-wdp <rakibul.wdp@gmail.com>
2024-12-02 04:59:14 +0600

    create blank home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-import { P, SubTitle, Title } from "@/components/typography";
+import { HomeHero } from "@/components/home";
 
 export default function Home() {
   return (
     <main>
-      <Title variant="H1">Title One</Title>
-      <Title variant="H2">Title One</Title>
-      <Title variant="H3">Title One</Title>
-      <Title variant="H4">Title One</Title>
-      <SubTitle variant="H4">SubTitle One</SubTitle>
-      <SubTitle variant="H5">SubTitle One</SubTitle>
-      <SubTitle variant="H6">SubTitle One</SubTitle>
-      <SubTitle variant="H7">SubTitle One</SubTitle>
-      <SubTitle variant="H8">SubTitle One</SubTitle>
-      <SubTitle variant="H9">SubTitle One</SubTitle>
-      <P variant="P1">Paragraph One</P>
-      <P variant="P2">Paragraph One</P>
-      <P variant="P3">Paragraph One</P>
-      <P variant="P4">Paragraph One</P>
-      hello world
+      <HomeHero />
     </main>
   );
 }
---
+++ src/components/home/home-hero.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const HomeHero: FC = () => {
+  return <div>home hero</div>;
+};

e576d22fd9ae25c4369c59db79aa3fe5d240b163
rakibul-wdp <rakibul.wdp@gmail.com>
2024-12-05 12:03:05 +0600

    your song your impact section correction

--- src/components/pages/pricing/your-song-impact.tsx
+++ src/components/pages/pricing/your-song-impact.tsx
@@ -7,8 +7,8 @@ import { FC } from "react";
 
 export const YourSongImpact: FC = () => {
   return (
-    <Container className="py-16 md:py-[220px]">
-      <div className={cn("relative py-[120px] lg:py-[160px]")}>
+    <Container className="">
+      <div className={cn("relative py-16 lg:py-[220px]")}>
         <div
           className="absolute inset-0 bg-center bg-no-repeat bg-cover"
           style={{
@@ -25,12 +25,14 @@ export const YourSongImpact: FC = () => {
         <div
           className={cn(
             "relative z-10",
-            "flex flex-col items-start justify-center gap-8 md:gap-16",
-            "px-4 md:px-0"
+            "flex flex-col items-start justify-center"
           )}
         >
           <Title variant="H4">YourSong. Your Impact.</Title>
-          <P variant="P1" className="text-[#A1A1A2]">
+          <P
+            variant="P1"
+            className="text-[#A1A1A2] md:w-[65%] mt-6 md:mt-8 mb-10 md:mb-12"
+          >
             The right song at the right moment has the power to change your
             life. Break free from conventional playlists and discover new talent
             in a novel way. By investing in emerging artists, you receive a
@@ -40,7 +42,7 @@ export const YourSongImpact: FC = () => {
           </P>
           <Link
             href="/impact"
-            className="text-xl leading-6 font-bold font-outfit text-primary-coral"
+            className="text-xl leading-6 font-bold font-outfit text-primary-coral flex items-center justify-center gap-x-1"
           >
             Learn More
             <ArrowRight className="" />

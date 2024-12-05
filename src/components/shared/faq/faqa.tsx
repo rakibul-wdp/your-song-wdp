96c0c9cccf0856d9beddf798aec3c9fff0e5eab7
rakibul-wdp <rakibul.wdp@gmail.com>
2024-12-05 09:07:06 +0600

    correction faq section and contact page done

--- src/components/shared/faq/faqa.tsx
+++ src/components/shared/faq/faqa.tsx
@@ -1,6 +1,8 @@
 import { TabPanel } from "@/components/common/tabs";
 import { P } from "@/components/typography";
-import { FC } from "react";
+import { cn } from "@/lib/utils";
+import { Minus, Plus } from "lucide-react";
+import { FC, useState } from "react";
 
 export interface IFAQS {
   id: number;
@@ -62,22 +64,44 @@ export const faqs: IFAQS[] = [
 ];
 
 export const FAQA: FC<{ activeIndex: number }> = ({ activeIndex }) => {
+  const [openQuestionId, setOpenQuestionId] = useState<number | null>(null);
+
+  const handleToggle = (id: number) => {
+    setOpenQuestionId((prevId) => (prevId === id ? null : id));
+  };
+
   return (
     <TabPanel
-      className="flex items-center justify-center gap-6"
+      className={cn(
+        "w-full flex flex-col items-start justify-center gap-2",
+        "p-0"
+      )}
       isActive={activeIndex === 1}
     >
       {faqs.map(({ id, question, answer }) => (
-        <div className="rounded-3xl" key={id}>
+        <div className="w-full bg-[#282829] rounded-xl py-4 px-6" key={id}>
           <div>
-            <P variant="P1" className="text-[#A1A1A2]">
-              {question}
-            </P>
-            <div className="flex items-center justify-between">
-              <P variant="P3" className="text-[#A1A1A2]">
-                {answer}
+            <div
+              className="flex items-center justify-between cursor-pointer delay-75 transition-all"
+              onClick={() => handleToggle(id)}
+            >
+              <P variant="P1" className="text-[#A1A1A2]">
+                {question}
               </P>
+              {openQuestionId === id ? (
+                <Minus className="text-[#A1A1A2]" />
+              ) : (
+                <Plus className="text-[#A1A1A2]" />
+              )}
             </div>
+
+            {openQuestionId === id && (
+              <div className="mt-2">
+                <P variant="P2" className="">
+                  {answer}
+                </P>
+              </div>
+            )}
           </div>
         </div>
       ))}
--- src/components/shared/faq/index.tsx
+++ src/components/shared/faq/index.tsx
@@ -6,9 +6,10 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/common/carousel";
-import { Tab, TabList, TabPanel, Tabs } from "@/components/common/tabs";
+import { Tab, TabList, Tabs } from "@/components/common/tabs";
 import { P, SubTitle } from "@/components/typography";
-import { ArrowRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
 import { FC, useState } from "react";
 import { FAQA } from "./faqa";
 
@@ -53,8 +54,13 @@ export const FAQ: FC = () => {
   };
 
   return (
-    <Container className="py-16 md:py-[120px]">
-      <div className="flex flex-col items-center justify-center gap-4">
+    <Container
+      className={cn(
+        "py-16 md:py-[120px]",
+        "flex flex-col items-center justify-center gap-10 md:gap-16"
+      )}
+    >
+      <div className="flex flex-col text-center items-center justify-center gap-4">
         <SubTitle variant="H5">Curious? Want to Learn More?</SubTitle>
         <P variant="P2" className="leading-10 text-[#A1A1A2]">
           Get answers to the most frequently asked questions about YourSong
@@ -62,31 +68,37 @@ export const FAQ: FC = () => {
         </P>
       </div>
 
-      <Tabs>
+      <Tabs className="w-full flex flex-col lg:flex-row items-start justify-between gap-4 lg:gap-20">
         <Carousel
           opts={{
             align: "start",
           }}
-          className="w-full md:hidden"
+          className="w-full lg:hidden"
         >
           <CarouselContent>
-            <TabList>
+            <TabList className="space-x-0">
               {categories.map(({ id, title }) => (
                 <CarouselItem key={id} className="basis-1/1">
                   <Tab
                     isActive={activeIndex === id}
                     onClick={() => setActiveIndex(id)}
+                    className="w-full bg-transparent p-0 m-0"
                   >
                     <div
                       onClick={handleToggle}
-                      className="flex items-center justify-between p-2 cursor-pointer rounded-md"
+                      className="flex items-center justify-between cursor-pointer rounded-md"
                     >
-                      <span className="text-lg font-medium">{title}</span>
-                      <ArrowRight
-                        className={`transition-transform ${
-                          activeIndex === id ? "" : "rotate-180"
-                        }`}
-                      />
+                      <P
+                        variant="P2"
+                        className={cn(
+                          "leading-8",
+                          activeIndex === id
+                            ? "text-primary-coral"
+                            : "text-[#5A5A5B]"
+                        )}
+                      >
+                        {title}
+                      </P>
                     </div>
                   </Tab>
                 </CarouselItem>
@@ -95,23 +107,47 @@ export const FAQ: FC = () => {
           </CarouselContent>
         </Carousel>
 
-        <TabList className="hidden md:block">
+        <TabList
+          className={cn(
+            "hidden lg:block min-w-[300px] space-x-0",
+            "lg:flex lg:flex-col lg:items-start lg:justify-center gap-4"
+          )}
+        >
+          <P variant="P2" className="text-[#A1A1A2] font-semibold">
+            Categories
+          </P>
           {categories.map(({ id, title }) => (
             <Tab
               isActive={activeIndex === id}
               onClick={() => setActiveIndex(id)}
               key={id}
+              className="w-full bg-transparent p-0 m-0"
             >
               <div
                 onClick={handleToggle}
-                className="flex items-center justify-between p-2 cursor-pointer rounded-md"
+                className="flex items-center justify-between cursor-pointer rounded-md"
               >
-                <span className="text-lg font-medium">{title}</span>
-                <ArrowRight
-                  className={`transition-transform ${
-                    activeIndex === id ? "" : "rotate-180"
-                  }`}
-                />
+                <P
+                  variant="P2"
+                  className={cn(
+                    "leading-8",
+                    activeIndex === id ? "text-primary-coral" : "text-[#5A5A5B]"
+                  )}
+                >
+                  {title}
+                </P>
+                <div
+                  className={cn(
+                    "p-0.5 rounded-full",
+                    activeIndex === id ? "bg-primary-coral" : "bg-[#383838]"
+                  )}
+                >
+                  <ChevronRight
+                    className={`transition-transform text-black h-5 w-5 ${
+                      activeIndex === id ? "" : "rotate-180"
+                    }`}
+                  />
+                </div>
               </div>
             </Tab>
           ))}
@@ -119,11 +155,11 @@ export const FAQ: FC = () => {
 
         {isOpen && activeIndex === 1 && <FAQA activeIndex={activeIndex} />}
 
-        {Array.from({ length: 5 }).map((_, index) => (
+        {/* {Array.from({ length: 5 }).map((_, index) => (
           <TabPanel isActive={activeIndex === index + 2} key={index}>
             <h2>Any content {index + 2}</h2>
           </TabPanel>
-        ))}
+        ))} */}
       </Tabs>
     </Container>
   );
